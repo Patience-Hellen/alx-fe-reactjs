9ba5b2afec12fe38557550cafac1c446da735d1a
Patience Hellen <patiencehellen91@gmail.com>
2025-09-03 18:23:02 +0300

    Updating App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import reactLogo from './assets/react.svg';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
 import WelcomeMessage from './components/WelcomeMessage';
 import viteLogo from '/vite.svg';
 
