27d2aea80a934ecda640784977c651c89aac2d2d
Patience Hellen <patiencehellen91@gmail.com>
2025-09-25 22:20:47 +0300

    Update Css

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,18 @@
 function UserProfile() {
     return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
         <img src="https://via.placeholder.com/150" 
         alt="User"
         className="w-36 h-36 mx-auto rounded-full mb-4" 
         />
-        <h1 className="text-x1 font-bold my-4 text-blue-800">John Doe</h1>
-        <p className="text-gray-600 text-base mt-2">Developer at Example Co. Loves to write code and explore new technologies.</p>
+        <h1 className="text-xl font-bold text-blue-800 my-4">
+            John Doe
+        </h1>
+        <p className="text-gray-600 text-base mt-2">
+            Developer at Example Co. Loves to write code and explore new technologies.
+        </p>
     </div>
     );
 }
 
 export default UserProfile;
-
