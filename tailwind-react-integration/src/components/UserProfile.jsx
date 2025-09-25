7303357ae2e5e3c6eee09f66092d561328871495
Patience Hellen <patiencehellen91@gmail.com>
2025-09-25 22:26:42 +0300

    Update TailwindCss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,16 @@
 function UserProfile() {
     return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
-        <img src="https://via.placeholder.com/150" 
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out text-center">
+        <img 
+        src="https://via.placeholder.com/150" 
         alt="User"
-        className="w-36 h-36 mx-auto rounded-full mb-4" 
+        className="w-24 h-24 md:w-36 md:h-36 mx-auto rounded-full mb-4 transform transition-transform duration-300 ease-in-out hover:scale-110" 
         />
-        <h1 className="text-xl font-bold text-blue-800 my-4">
-            John Doe
+        <h1 className="text-lg md:text-xl font-bold text-blue-800 my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
+        John Doe
         </h1>
-        <p className="text-gray-600 text-base mt-2">
-            Developer at Example Co. Loves to write code and explore new technologies.
+        <p className="text-sm md:text-base text-gray-600 mt-2">
+        Developer at Example Co. Loves to write code and explore new technologies.
         </p>
     </div>
     );
