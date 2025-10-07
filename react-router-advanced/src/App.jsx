782f0b24c1699a5baa0180b65578bd0a9fa8e612
Patience Hellen <patiencehellen91@gmail.com>
2025-10-07 18:57:16 +0300

    Update router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,16 +1,25 @@
 // src/App.jsx
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import BlogPost from "./components/BlogPost";
-import Home from "./components/Home";
+import HomePage from "./components/HomePage";
 import Profile from "./components/Profile";
+import ProtectedRoute from "./components/ProtectedRoute"; // ✅ make sure this line exists
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/blog/:id" element={<BlogPost />} />
+        {/* ✅ Protected Route usage */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
