7716cf444b2fdb7ce1fe11215410ffd182f6b5ff
Patience Hellen <patiencehellen91@gmail.com>
2025-10-08 15:08:28 +0300

    u

--- react-todo/src/components/TodoLis.jsx
+++ react-todo/src/components/TodoLis.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const TodoList = () => {
+const TodoLis = () => {
   const [todos, setTodos] = useState([
     { id: 1, text: "Learn React", completed: false },
     { id: 2, text: "Build a project", completed: true },
